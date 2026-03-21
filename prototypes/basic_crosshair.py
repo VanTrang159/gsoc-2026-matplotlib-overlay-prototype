e08416ac83bf279c27beb38483741f630d23df03
VanTrang159 <vanminhtrangyt@gmail.com>
2026-03-22 02:22:26 +0700

    Create basic_crosshair.py

---
+++ prototypes/basic_crosshair.py
@@ -0,0 +1,33 @@
+class OverlayElement:
+    def update(self, event):
+        pass
+
+    def draw(self, renderer):
+        pass
+
+
+class OverlayManager:
+    def __init__(self):
+        self.elements = []
+
+    def add(self, element):
+        self.elements.append(element)
+
+    def remove(self, element):
+        self.elements.remove(element)
+
+    def update_all(self, event):
+        for element in self.elements:
+            element.update(event)
+
+    def draw_all(self, renderer):
+        for element in self.elements:
+            element.draw(renderer)
+
+
+class CrosshairOverlay(OverlayElement):
+    pass
+
+
+class CoordinateOverlay(OverlayElement):
+    pass
